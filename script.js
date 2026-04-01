10c3f1018b80fadd3b9879cddfa737f418a482d2
Daniel-Perger <daniel.perger@outlook.de>
2026-04-01 16:41:32 +0200

    Die Grundstruktur der Website.
    Beinhaltet sehr einfaches HTML, JavaScript und CSS Code.

---
+++ script.js
@@ -0,0 +1,7 @@
+function zeigeLebenslauf() {
+    document.getElementById("content").innerHTML = `
+        <h2>Lebenslauf</h2>
+        <p>Schule: HTL...</p>
+        <p>Skills: Java, HTML, CSS</p>
+    `;
+}
