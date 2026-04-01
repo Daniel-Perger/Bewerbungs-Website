a69943f32d1e27f25f0b3efbe8b8dc50ff125687
Daniel-Perger <daniel.perger@outlook.de>
2026-04-01 19:54:26 +0200

    Lebenslauf
    Lebenslauf geringfügig erweitert.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function zeigeLebenslauf() {
     document.getElementById("content").innerHTML = `
         <h2>Lebenslauf</h2>
-        <p>Schule: HTL...</p>
-        <p>Skills: Java, HTML, CSS</p>
     `;
+	document.getElementById("extraButton").style.display = "block";
+	
+	document.getElementById("extraButton").innerHTML = `
+		<button onclick="zeigeSchule()">
+            Schulische Ausbildung anzeigen
+        </button>
+    `;
+
+function zeigeSchule() {
+	document.getElementById("extraButton").innerHTML = `
+		<h2>Schulische Ausbildung</h2>
+		<p>09/2025 bis 03/2026: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
+		<p>09/2024 bis 01/2025: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
+		<p>09/2022 bis 06/2023: Handelsakademie, Neunkirchen</p>
+		<p>09/2018 bis 07/2022: Bilingual Junior High School, Wiener Neustadt</p>
+	`;
 }
