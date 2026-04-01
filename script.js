311e7e8cb608bc057bb2559ed9bea697df7652ef
Daniel-Perger <daniel.perger@outlook.de>
2026-04-01 20:28:12 +0200

    Text Linksbündig

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function zeigeLebenslauf() {
         <h2>Lebenslauf</h2>
     `;
 	document.getElementById("extraButton").style.display = "block";
+	
 	document.getElementById("extraButton").innerHTML = `
 		<button onclick="zeigeSchule()">
             Schulische Ausbildung anzeigen
@@ -12,10 +13,11 @@ function zeigeLebenslauf() {
 
 function zeigeSchule() {
 	document.getElementById("extraButton").innerHTML = `
-		<h2>Schulische Ausbildung</h2>
-		<p>09/2025 bis 03/2026: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
-		<p>09/2024 bis 01/2025: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
-		<p>09/2022 bis 06/2023: Handelsakademie, Neunkirchen</p>
-		<p>09/2018 bis 07/2022: Bilingual Junior High School, Wiener Neustadt</p>
+		<div class="left-text">
+			<h2>Schulische Ausbildung</h2>
+			<p>09/2025 bis 03/2026: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
+			<p>09/2024 bis 01/2025: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
+			<p>09/2022 bis 06/2023: Handelsakademie, Neunkirchen</p>
+			<p>09/2018 bis 07/2022: Bilingual Junior High School, Wiener Neustadt</p>
 	`;
 }
