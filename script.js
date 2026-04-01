a71ba83e8cdc72a86e45c8104a0dde4ecdbb25f4
Daniel-Perger <daniel.perger@outlook.de>
2026-04-01 21:16:24 +0200

    Optimierung
    Optimierung am Code / kleine Änderung an den Buttons (Text ein- ausklappbar).

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-function zeigeLebenslauf() {
-    document.getElementById("content").innerHTML = `
-        <h2>Lebenslauf</h2>
-    `;
-	document.getElementById("extraButton").style.display = "block";
-	
-	document.getElementById("extraButton").innerHTML = `
-		<button onclick="zeigeSchule()">
-            Schulische Ausbildung anzeigen
-        </button>
-    `;
-}
-
-function zeigeSchule() {
-	document.getElementById("extraButton").innerHTML = `
-		<div class="left-text">
-			<h2>Schulische Ausbildung</h2>
-			<p>09/2025 bis 03/2026: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
-			<p>09/2024 bis 01/2025: Schule für allgemeine Gesundheits- und Krankenpflege, Neunkirchen</p>
-			<p>09/2022 bis 06/2023: Handelsakademie, Neunkirchen</p>
-			<p>09/2018 bis 07/2022: Bilingual Junior High School, Wiener Neustadt</p>
-	`;
+function toggleText(id) {
+	const element = document.getElementById(id);
+	 if (element.style.display === "block") {
+		 element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
 }
