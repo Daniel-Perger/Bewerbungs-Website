50815bfbe001d77e7143c0c47982150c0fdccaeb
Daniel-Perger <daniel.perger@outlook.de>
2026-04-02 10:28:27 +0200

    Update script.js
    Die hinzugefügten Buttons funktionieren nicht; ich probier nur was

--- script.js
+++ script.js
@@ -11,5 +11,7 @@ function onceButtons(id) {
 	const element = document.getElementById(id);
 		if (element.style.display === "block") {
 			element.style.display = "none";
-		}
+		} else {
+        element.style.display = "block";
+    }
 }
